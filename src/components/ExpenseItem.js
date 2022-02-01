df09ff2f1ae578f857e0ca9308d93806ad2aa1e7
Sharryy <ibneadam388@gmail.com>
2022-02-01 16:18:03 +0500

    Added Props

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,16 +1,12 @@
 import './ExpenseItem.css'
 
-function ExpenseItem() {
-    const expenseDate = new Date(2020, 0o2, 0o2);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = '100';
-
+function ExpenseItem(props) {
     return (
         <div className="expense-item">
-            <div>{expenseDate.toDateString()}</div>
+            <div>{props.date.toDateString()}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">${expenseAmount}</div>
+                <h2>{props.title}</h2>
+                <div className="expense-item__price">${props.amount}</div>
             </div>
         </div>
     );
