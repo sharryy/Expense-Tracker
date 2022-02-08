b805c7a0bb2c0145a359f0e8af27932c599afc67
Sharryy <ibneadam388@gmail.com>
2022-02-08 18:11:33 +0500

    Done with Chart Bar

--- src/components/Expense/Expense.js
+++ src/components/Expense/Expense.js
@@ -3,6 +3,7 @@ import Card from "../UI/Card";
 import ExpenseFilter from "./ExpenseFilter";
 import {useState} from "react";
 import ExpenseList from "./ExpenseList";
+import ExpenseChart from "./ExpenseChart";
 
 function Expense(props) {
     const [filterYear, setFilterYear] = useState('2022');
@@ -15,6 +16,7 @@ function Expense(props) {
     return (
         <Card className='expenses'>
             <ExpenseFilter date={filterYear} onChangeFilterYear={changeFilterYearHandler}/>
+            <ExpenseChart expenses={props.items}/>
             <ExpenseList items={props.items}/>
         </Card>
     );
